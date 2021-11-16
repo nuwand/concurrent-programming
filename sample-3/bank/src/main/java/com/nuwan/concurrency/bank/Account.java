d2af4023e8c38039d3ea86953008d7e68f1131ce
Nuwan Dias <nuwand@wso2.com>
2021-11-16 22:57:48 +0530

    Fixing sample 3

--- sample-3/bank/src/main/java/com/nuwan/concurrency/bank/Account.java
+++ sample-3/bank/src/main/java/com/nuwan/concurrency/bank/Account.java
@@ -2,4 +2,25 @@ package com.nuwan.concurrency.bank;
 
 public class Account {
 
+    String accountId;
+
+    int balance;
+
+    public Account(String accountId, int balance) {
+        this.accountId = accountId;
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public boolean canWithdraw(int amount) {
+        return balance - amount > 0;
+    }
+
 }
--- sample-3/bank/src/main/java/com/nuwan/concurrency/bank/BankingApp.java
+++ sample-3/bank/src/main/java/com/nuwan/concurrency/bank/BankingApp.java
@@ -1,8 +1,46 @@
 package com.nuwan.concurrency.bank;
 
-public class BankingApp extends Thread {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BankingApp {
+
+    static Map<String, Account> accounts = new HashMap<String, Account>();
 
     public static void main(String[] args) {
+
+        Account newAccount = new Account("12345", 100);
+        accounts.put(newAccount.accountId, newAccount);
+
+        String accountId = "";
+        int withdrawalAmount;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            while (!accountId.equals("exit")) {
+                System.out.println("Enter account id : ");
+                accountId = bufferedReader.readLine();
+
+                Account account = accounts.get(accountId);
+                if (account == null) {
+                    throw new Exception("No account with ID found");
+                }
+                System.out.println("Your account balance is : " + account.getBalance());
+                System.out.println("Enter withdrawal amount : ");
+                withdrawalAmount = Integer.parseInt(bufferedReader.readLine());
+
+                if(!account.canWithdraw(withdrawalAmount)) {
+                    throw new Exception("Insufficient funds!");
+                }
+
+                account.setBalance(account.getBalance() - withdrawalAmount);
+                System.out.println("New account balance is : " + account.getBalance());
+            }
+        } catch (Exception e) {
+            System.out.println("Error " + e.getMessage());
+        }
 
     }
 
